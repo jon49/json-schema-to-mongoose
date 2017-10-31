58c9a9a7e657074a5f1b80b71ac285e6385f27a6
agrozyme <agrozyme@gmail.com>
2017-11-01 02:42:14 +0800

    fix export

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// export * from './lib/json-schema';
 var json_schema_1 = require("./lib/json-schema");
-exports.createMongooseSchema = json_schema_1.default;
+module.exports = json_schema_1.default;
+// export = createMongooseSchema; 
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
-﻿import createMongooseSchema from './lib/json-schema';
+﻿// export * from './lib/json-schema';
+import createMongooseSchema from './lib/json-schema';
 
-export {createMongooseSchema};
+module.exports = createMongooseSchema;
+// export = createMongooseSchema;
